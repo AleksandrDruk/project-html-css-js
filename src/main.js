93a1fec6d8987c63f6fd836e0d5d73298ee3232f
AleksandrDruk <aleksandr.druk.ru@gmail.com>
2024-03-02 23:37:48 +0300

    upd

--- src/main.js
+++ src/main.js
@@ -157,28 +157,3 @@ document.addEventListener('DOMContentLoaded', () => {
   calcScrollValue();
   window.onscroll = calcScrollValue;
 });
-
-$('.slick-slider').slick({
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 2000,
-  responsive: [
-    {
-      breakpoint: 1280,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
-      },
-    },
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        infinite: true,
-        dots: true,
-      },
-    },
-  ],
-});
